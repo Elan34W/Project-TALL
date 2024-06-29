08e8cf076cc56c34c06628559aaf30119c099880
Resyorc <blaskishare@gmail.com>
2024-06-29 08:03:49 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel TALL Stack</title>
+    @livewireStyles
+</head>
+<body>
+    @yield('content')
+    @livewireScripts
+</body>
+</html>
+```     
---
+++ resources/views/livewire/example.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Hello, TALL Stack!</h1>
+</div>
